df785b8247679ceb45a3ffc84328cea1a959f837
Sergio Caballero <cbsergioprof@gmail.com>
2026-06-04 09:35:08 +0200

    realización del ejercicio 1a1

--- 1a/ej1a1.py
+++ 1a/ej1a1.py
@@ -1,15 +1,17 @@
 '''
 Enunciado:
-Implementa la función 'fibonacci(fibonacci_number)' que contenga el algoritmo
-de Fibonacci y reciba como parámetro un valor numérico entero llamado 
-'fibonacci_number'  y devuelva el valor de la serie Fibonacci en esa posición.
+Implementa la función 'fibonacci(fibonacci_number)' 
+    que contenga el algoritmo de Fibonacci 
+    y reciba como parámetro un valor numérico entero llamado 'fibonacci_number'  
+    y devuelva el valor de la serie Fibonacci en esa posición.
+
 Asimismo, si el valor no es numérico, o es menor a cero, se debe lanzar 
-una excepción ValueError("mensaje"), la cual puede incluir un mensaje que debería 
-corresponder con el tipo de error durante la validación.
+    una excepción ValueError("mensaje"), 
+        la cual puede incluir un mensaje que debería corresponder con 
+            el tipo de error durante la validación.
 
 Parámetros:
-- fibonacci_number: Número entero positivo mayor a 0 que representa la
-posición en la serie Fibonacci.
+- fibonacci_number: Número entero positivo mayor a 0 que representa la posición en la serie Fibonacci.
 
 Ejemplo:
     Entrada:
@@ -40,9 +42,24 @@ Exemple:
 '''
 
 def fibonacci(fibonacci_number):
-    # Write here your code
+    x = 0
+    y = 1
+
+    if not isinstance(fibonacci_number, int):
+        raise ValueError("El valor introducido debe ser un número entero")
+    elif fibonacci_number < 0:
+        raise ValueError("El número no puede ser menor a 0")
+    elif fibonacci_number == 0:
+        return 0
+    elif fibonacci_number == 1:
+        return 1
+    
+    for n in range(2, fibonacci_number + 1):
+        x, y = y, x + y
+    return y
+    
     pass
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(fibonacci(10))
+print(fibonacci(10))
